dcda1c0d288622363725f959d1d86fcef5a941e0
sonikenakhre <116624601+sonikenakhre@users.noreply.github.com>
2023-01-30 17:19:49 +0530

    Create TransactionConstants.java

---
+++ TransactionConstants.java
@@ -0,0 +1,38 @@
+package com.anz.rtl.transactions.constants;
+
+public class TransactionConstants {
+
+	public static final String SERVICE_NAME = "RTLGateway";
+	public static final String REQUEST_ID = "request.id";
+	public static final String CHANNEL = "channel";
+	public static final String PRODUCT_TYPE = "product.type";
+	public static final String ACCOUNT_ID = "account.id";
+	public static final String REQUEST_CURSOR = "request.cursor";
+	public static final String SERVICE_FLAG = "service.flag";
+	public static final String PRIORDAY_TIME = "priorday.time";
+	public static final String BLINDSPOT_TIME = "blindspot.time";
+	public static final String INTRADAY_TIME = "intraday.time";
+	public static final String OUTSTANDING_TIME = "outstanding.time";
+	public static final String MERGE_TIME = "merge.time";
+	public static final String SORT_TIME = "sort.time";
+	public static final String REDIS_INSERT = "redis.insert";
+	public static final String REDIS_RETRIEVAL = "redis.retrieval";
+	public static final String REDIS_RECORD_COUNT = "redis.record.count";
+	public static final String RESPONSE_RECORD_SENT = "response.record.sent";
+	public static final String RESPONSE_CURSOR = "response.cursor";
+	public static final String START_DATE = "start.date";
+	public static final String END_DATE = "end.date";
+	public static final String STATUS = "status";
+	public static final String TOTAL_TIMETAKEN = "total.timeTaken";
+	public static final String PRIORDAY_RETRY_FLAG = "priorday.retry.flag";
+	public static final String PRIORDAY_RETRY_TIME = "priorday.retry.time";
+	public static final String TRANSACTION_ENRICH = "txnEnrich.time";
+	public static final String MERCHANT_INFO_FLG = "merchant.info.flag";
+
+	public static final String BPAY = "BPAY";
+	public static final String DIRECTDEBIT = "DIRECT_DEBIT";
+	public static final String POS = "POS";
+	public static final String CARD = "CARD";
+	public static final String ATM = "ATM";
+
+}
